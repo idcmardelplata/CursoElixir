ca369cd8237aec498322aaae5c22a2de8e010fb9
Martin Algañaraz <idcmardelplata@gmail.com>
2021-02-17 14:45:22 -0300

    refactor Mates module with a struct

--- lib/calculadora.ex
+++ lib/calculadora.ex
@@ -1,17 +1,20 @@
 defmodule Mates do
+  alias Mates.Operation
+
   def suma(x, y) do
-    Mates.Private.operar({:suma, x, y})
+    Mates.Private.operar(%Operation{a: x, b: y, operation: &+/2})
+    22 + 11
   end
 
   def resta(x, y) do
-    Mates.Private.operar({:resta, x, y})
+    Mates.Private.operar(%Operation{a: x, b: y, operation: &-/2})
   end
 
   def multiplicar(x, y) do
-    Mates.Private.operar({:multiplicar, x, y})
+    Mates.Private.operar(%Operation{a: x, b: y, operation: &*/2})
   end
 
   def dividir(x, y) when x > 0 and y > 0 do
-    Mates.Private.operar({:division, x, y})
+    Mates.Private.operar(%Operation{a: x, b: y, operation: &div/2})
   end
 end
