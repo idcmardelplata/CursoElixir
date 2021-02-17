459f87533c3483a35681cfccc6b5a17183a0f445
Martin Algañaraz <idcmardelplata@gmail.com>
2021-02-17 14:45:22 -0300

    refactor Mates module with a struct

--- lib/calculadora/calculadora_inter.ex
+++ lib/calculadora/calculadora_inter.ex
@@ -1,6 +1,5 @@
 defmodule Mates.Private do
-  def operar({:suma, x, y}), do: x + y
-  def operar({:resta, x, y}), do: x - y
-  def operar({:multiplicar, x, y}), do: x * y
-  def operar({:division, x, y}), do: div(x, y)
+  alias Mates.Operation
+
+  def operar(%Operation{a: a, b: b, operation: ope}), do: ope.(a, b)
 end
