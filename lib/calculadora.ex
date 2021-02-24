cd31accc4e5078e0f6a25e372d9087476691187d
Martin Algañaraz <idcmardelplata@gmail.com>
2021-02-24 17:46:30 -0300

    some changes

--- lib/calculadora.ex
+++ lib/calculadora.ex
@@ -1,11 +1,30 @@
 defmodule Mates do
+  @moduledoc """
+  Modulo basico con algunas funciones matematicas.
+
+  Esto es realmente innecesario porque elixir ya posee funciones
+  para realizar operaciones matematicas.
+
+  ## Example
+  iex> Mates.suma(25,11)
+  36
+  """
+
   alias Mates.Operation
 
+  @doc """
+  add `x` and `y` and return the result of the suma. 
+  """
   def suma(x, y) do
     Mates.Private.operar(%Operation{a: x, b: y, operation: &+/2})
-    22 + 11
   end
 
+  @doc """
+  resta dos numeros.
+  ## Examples
+  iex> Mates.resta(22,11)
+  11
+  """
   def resta(x, y) do
     Mates.Private.operar(%Operation{a: x, b: y, operation: &-/2})
   end
--- test/mates_test.exs
+++ test/mates_test.exs
@@ -1,8 +1,4 @@
 defmodule MatesTest do
   use ExUnit.Case
   doctest Mates
-
-  test "greets the world" do
-    assert Mates.hello() == :world
-  end
 end
